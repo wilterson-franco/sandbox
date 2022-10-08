c552c83c01fa49e9d008392bd48c0bfef1498d37
Wilterson Franco <wilterson.franco@hotmail.com>
2022-10-08 17:23:12 -0400

    Properly handles 404

--- src/main/java/com/wilterson/tutorials/springcachewebapp/controller/BookController.java
+++ src/main/java/com/wilterson/tutorials/springcachewebapp/controller/BookController.java
@@ -3,10 +3,12 @@ package com.wilterson.tutorials.springcachewebapp.controller;
 import com.wilterson.tutorials.springcachewebapp.model.Book;
 import com.wilterson.tutorials.springcachewebapp.service.BookService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @AllArgsConstructor
 @RestController
@@ -17,7 +19,7 @@ public class BookController {
     @GetMapping("/books/{isbn}")
     public ResponseEntity<Book> getBookByIsbn(@PathVariable String isbn) {
 
-        Book book = bookService.getBookByIsbn(isbn).orElseThrow(() -> new RuntimeException("Book not found"));
+        Book book = bookService.getBookByIsbn(isbn).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Book not found"));
 
         return ResponseEntity
                 .ok()
